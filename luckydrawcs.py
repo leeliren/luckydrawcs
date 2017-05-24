2320b8d6f7c8c1953ac40276185e9ca434372e52
leeliren <lee.liren@dhs.sg>
2017-05-25 02:16:09 +0800

    Add files via upload

---
+++ luckydrawcs.py
@@ -0,0 +1,43 @@
+import csv
+import random
+import time
+with open('BuildingBloCS.csv',newline="") as csvfile:
+    file = csv.reader(csvfile,delimiter = ',')
+    occupation = []
+    name = []
+    school = []
+
+    for row in file:
+        occupation.append(row[5])
+        name.append(row[2])
+        school.append(row[3])
+
+    def luckydraw():
+        winner = random.randint(1,len(name)-1)
+        if occupation[winner] == "Student":
+            return name[winner], school[winner]
+        elif occupation[winner] == "Teacher": #teachers cant win?
+            return luckydraw()
+
+    def kindlewinner():
+        winner = random.randint(1,13)
+        if occupation[winner] == "Student":
+            return name[winner], school[winner]
+        elif occupation[winner] == "Teacher": #teachers cant win?
+            return kindlewinner()
+        
+    def display(prize):
+        if prize == "luckydraw":
+            print("Winner is ",end = "")
+            winner = luckydraw()[0]
+            school = luckydraw()[1]
+        elif prize == "kindle":
+            print("Kindle winner is ",end = "")
+            winner = kindlewinner()[0]
+            school = kindlewinner()[1]
+        for i in range (4):
+            time.sleep(.5)
+            print(".",end = "")
+        print(winner,"from", school, "!!")
+
+    display("kindle")
